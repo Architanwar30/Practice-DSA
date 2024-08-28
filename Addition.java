177dc05e7afb2b60ed4321e06b6f4205c9bc459a
Architanwar30 <architanwar30@gmail.com>
2024-08-28 13:13:27 +0530

    Addition_of_two_no.

--- Addition.java
+++ Addition.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 public class Addition {
     public static void main(String[] args) {
         int a,b,c;
-        Scanner sc= new Scanner(System.sc);
+        Scanner sc= new Scanner(System.in);
 
-        x=sc.nextInt();
-        y=sc.nextInt();
+        System.out.println("Enter 1st no.");
+        a=sc.nextInt();
+        System.out.println("Enter 2nd no.");
+        b=sc.nextInt();
         c=a+b;
         System.out.println("Sum of two numbers:"+c);
 
