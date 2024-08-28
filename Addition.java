922e2e344d4be27cbe6072d3324a5bd565657693
Architanwar30 <architanwar30@gmail.com>
2024-08-28 12:49:39 +0530

    Addition_of_two_no.

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc= new Scanner(System.sc);
+
+        x=sc.nextInt();
+        y=sc.nextInt();
+        c=a+b;
+        System.out.println("Sum of two numbers:"+c);
+
+    }
+}
